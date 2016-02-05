f19555113ea65828251f1eb5266dbd8710da8110
Ankit <ankit.bharat.tanna@accenture.com>
2016-02-05 13:07:25 +0530

    EP: Added Rating Directive
    EP: Added Rating Directive

---
+++ www/js/controllers/ratingController.js
@@ -0,0 +1,11 @@
+angular.module('EmployeePanda.controllers')
+    .controller('RatingController', RatingController);
+  function RatingController() {
+      console.log('Rating Controller Called');
+    this.rating1 = 5;
+    this.rating2 = 2;
+    this.isReadonly = true;
+    this.rateFunction = function(rating) {
+      console.log('Rating selected: ' + rating);
+    };
+  }
