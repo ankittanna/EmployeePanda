fecfd941ef4d216265ab0b485f15771164300c49
Ankit <ankit.bharat.tanna@accenture.com>
2016-02-08 15:32:03 +0530

    EP: Calculating Order Cost
    EP: Calculating Order Cost

--- www/js/controllers/vendorMenuController.js
+++ www/js/controllers/vendorMenuController.js
@@ -4,17 +4,41 @@ angular.module('EmployeePanda.controllers')
      
      // Fetch Vendor Information
      this.vendorInfo = DetailsService.vendorInfo.selectedVendor.get('selectedVendorObject');
-	
+	 
+	 // Explicit Menu List
+	 this.vendorMenu = this.vendorInfo.menu;
+
+	 this.orderCost = 0;
+
+	 this.updateOrderQuantity = function()
+	 {
+	 	this.orderCost = this.calculateOrderCost();
+	 };
+
 	 this.decreaseCounter = function(counterId){
 	 	if(parseInt(angular.element('#counter'+counterId).val())>0){
 	 		angular.element('#counter'+counterId).val(parseInt(angular.element('#counter'+counterId).val()) -1);
+	 		this.vendorInfo.menu[counterId].quantity = parseInt(angular.element('#counter'+counterId).val());
 	 	}
+	 	this.orderCost = this.calculateOrderCost();
 	 };
 
 	 this.increaseCounter = function(counterId){
 	 	if(parseInt(angular.element('#counter'+counterId).val())<50){
 	 		angular.element('#counter'+counterId).val(parseInt(angular.element('#counter'+counterId).val()) +1);
+	 		this.vendorInfo.menu[counterId].quantity = parseInt(angular.element('#counter'+counterId).val());
 	 	}
+	 	this.orderCost = this.calculateOrderCost();
+	 };
+
+	 this.calculateOrderCost = function(){
+	 	var itemsCostForOrder = this.vendorInfo.menu.map(function(item, index, array){
+	 		return parseInt(item.rate)*parseInt(item.quantity);
+	 	});
+
+	 	var totalCost = itemsCostForOrder.reduce(function(a, b) { return a + b; }, 0);
+
+	 	return parseInt(totalCost) || 0;
 	 };
 
 });
