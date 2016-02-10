3227a718a005831057cfb6c323572c84c7ee17f9
Ankit <ankit.bharat.tanna@accenture.com>
2016-02-10 15:03:30 +0530

    Make Menu Dynamic based on user role
    This change makes the side menu dynamic based on role of the employee

--- www/js/controllers/appMenuController.js
+++ www/js/controllers/appMenuController.js
@@ -1,6 +1,9 @@
 angular.module('EmployeePanda.controllers')
-.controller('AppMenuController', function($scope, $state) {  
+.controller('AppMenuController', function($scope, $state, DetailsService) {  
     this.goToPage = function(state){
         $state.go(state);
     };
+
+    this.loginInfo = DetailsService.loginInfo.userInfo.get();
+    //alert(JSON.stringify(this.loginInfo));
 });
