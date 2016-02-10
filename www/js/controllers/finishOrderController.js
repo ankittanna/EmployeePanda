c04b53e167a4a44875f10d2c917b6ef4e82b57fc
Ankit <ankit.bharat.tanna@accenture.com>
2016-02-10 10:08:53 +0530

    EP: QR Code Generator
    EP: QR Code Generator - Action undefined

--- www/js/controllers/finishOrderController.js
+++ www/js/controllers/finishOrderController.js
@@ -2,5 +2,11 @@ angular.module('EmployeePanda.controllers')
 .controller('FinishOrderController', function($scope, $stateParams, EmployeeService, DetailsService, $state) {  
     this.orderDetails = DetailsService.employeeOrder.employeeOrder.get();
 
+    this.backToHome = function(){
+        $state.go('app.vendorList');
+    };
     
+    angular.element('#orderInfo').qrcode({
+        text    : "http://jetienne.com"
+    });;
 });
